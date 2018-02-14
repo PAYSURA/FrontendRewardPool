d7bae2a3aadf69cd366049c9976fb20f333ab8fb
Kratersk88 <a.baklizki@gmx.de>
2018-02-14 18:21:16 +0100

    Model added
    * Product model has been added

--- paysura/src/app/app.component.ts
+++ paysura/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import {Product} from '../app/product/product.model';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'app';
+
+@Component( {
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+} )
+export class AppComponent{
+    title = 'app';
+
+    elemente = [
+                new Product('produkt1',"lorem ipsum",'img/test.png');
+    ];
 }
