142b325f6132164163f01693026e3de6204061f2
Kratersk88 <a.baklizki@gmx.de>
2018-02-18 18:26:47 +0100

    UI
    Css Updates

--- paysura/src/app/product/product.component.ts
+++ paysura/src/app/product/product.component.ts
@@ -10,10 +10,10 @@ export class ProductComponent implements OnInit {
 
     ipc_logo = 'assets/img/rsz_coin_logo.png';
     public carouselElements = [
-        new Product('Samsung 4K TV', 'lorem ipsum', '', 690, 700),
-        new Product('IPhone X', 'lorem ipsum', '', 990, 1000),
-        new Product('Women - Sweater', 'lorem ipsum', '', 70, 80),
-        new Product('Playstation 4 Pro', 'lorem ipsum', '', 390, 400)
+        new Product('Samsung 4K TV', 'lorem ipsum', 'assets/img/rsz_coin_logo.png', 690, 700),
+        new Product('IPhone X', 'lorem ipsum', 'assets/img/rsz_coin_logo.png', 990, 1000),
+        new Product('Women - Sweater', 'lorem ipsum', 'assets/img/rsz_coin_logo.png', 70, 80),
+        new Product('Playstation 4 Pro', 'lorem ipsum', 'assets/img/rsz_coin_logo.png', 390, 400)
     ];
 
     constructor() {
