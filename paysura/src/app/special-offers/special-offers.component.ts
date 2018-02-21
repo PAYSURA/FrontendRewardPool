bdf5beeca6884435215a765006eedf6347f1788f
Kratersk88 <a.baklizki@gmx.de>
2018-02-21 20:13:32 +0100

    Special Offers
    UI Updated

--- paysura/src/app/special-offers/special-offers.component.ts
+++ paysura/src/app/special-offers/special-offers.component.ts
@@ -9,11 +9,11 @@ import {SpecialOffer} from './specialOffer.model';
 export class SpecialOffersComponent implements OnInit {
 
     specialOffers = [
-        new SpecialOffer('Digital', '#000000'),
-        new SpecialOffer('Explore the Car Sharing', '#000000'),
-        new SpecialOffer('3 Month Music Stream', '#000000'),
-        new SpecialOffer('Movies & Series', '#000000'),
-        new SpecialOffer('Donate your IPC', '#000000'),
+        new SpecialOffer('Digital', '#2E9AFE'),
+        new SpecialOffer('Explore the Car Sharing', '#0040FF'),
+        new SpecialOffer('3 Month Music Stream', '#01DF3A'),
+        new SpecialOffer('Movies & Series', '#1C1C1C'),
+        new SpecialOffer('Donate your IPC', '#8A0808'),
     ];
 
 
