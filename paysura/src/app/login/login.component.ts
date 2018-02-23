9ccb909a7b01702af566fcd2c4ae5c7fef188d93
Kratersk88 <a.baklizki@gmx.de>
2018-02-23 19:00:20 +0100

    Login
    login has been updated/created

--- paysura/src/app/login/login.component.ts
+++ paysura/src/app/login/login.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from '../model/user/user.model';
+import {UserService} from '../user.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css'],
+    providers: [UserService]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    loginUser: User = new User();
+    loader: boolean;
 
-  ngOnInit() {
-  }
+    constructor(private userService: UserService) {
+    }
+
+    private showLoader() {
+        this.loader = true;
+    }
+
+    private hideLoader() {
+        this.loader = false;
+    }
+
+    public login() {
+        if (this.loginUser.password.length > 0 && this.loginUser.email.length > 0) {
+            this.showLoader();
+            const result = this.userService.findUser(this.loginUser.email, this.loginUser.password);
+            if (result == null) {
+                alert('Invalid login');
+                this.hideLoader();
+            } else {
+                const func = this;
+                setTimeout(function () {
+                    window.sessionStorage.setItem('logged', '1');
+                    window.sessionStorage.setItem('firstname', result.firstname);
+                    window.sessionStorage.setItem('lastname', result.lastname);
+                    window.sessionStorage.setItem('username', result.username);
+                    window.sessionStorage.setItem('email', result.email);
+                    func.hideLoader();
+                    alert(1);
+                }, 1000);
+
+            }
+        } else {
+            this.hideLoader();
+        }
+    }
+
+    ngOnInit() {
+    }
 
 }
--- paysura/src/app/user.service.ts
+++ paysura/src/app/user.service.ts
@@ -4,17 +4,33 @@ import {User} from './model/user/user.model';
 @Injectable()
 export class UserService {
 
-    private users: User[] = [];
-
     constructor() {
     }
 
     public addUser(data: any) {
-        this.users.push(data);
+        let arr = this.getLocalData();
+        if (arr == null) {
+            arr = new Array();
+        }
+        arr.push(data);
+        localStorage.setItem('users', JSON.stringify(arr));
+    }
+
+    private getLocalData() {
+        const _data = localStorage.getItem('users');
+        return JSON.parse(_data);
     }
 
     public findUser(email: string, password: string) {
-
+        let result: User = null;
+        const users = this.getLocalData();
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].email === email && users[i].password === password) {
+                result = users[i];
+            }
+        }
+        console.log(result);
+        return result;
     }
 
 }
